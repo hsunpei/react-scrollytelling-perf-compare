8325921aefb4483124d8c68f3e24f77d445f6f66
HsunPei Wang <garfieldduck@gmail.com>
2024-11-09 01:38:13 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App bg-blue-300">
+      <h1>React + TypeScript + TailwindCSS</h1>
+    </div>
+  );
+}
