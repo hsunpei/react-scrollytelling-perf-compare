c649c52aca30ad9879f44fd377d2bd776b7dec31
Hsun-Pei Wang <garfieldduck@gmail.com>
2024-11-22 01:21:07 +0100

    🚧 Set up the male characters

--- src/BackgroundGraphics.tsx
+++ src/BackgroundGraphics.tsx
@@ -1,16 +1,20 @@
+import { BgTopRight } from './illustrations/BgTopRight';
 import { LeftMan } from './illustrations/LeftMan';
+import { RightMan } from './illustrations/RightMan';
 
 export const BackgroundGraphics = () => {
   return (
     <svg
       width="1234"
       height="832"
-      className="h-full w-full object-cover"
+      className="fill-box h-full w-full object-cover"
       viewBox="0 0 1234 832"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <BgTopRight />
       <LeftMan />
+      <RightMan />
     </svg>
   );
 };
