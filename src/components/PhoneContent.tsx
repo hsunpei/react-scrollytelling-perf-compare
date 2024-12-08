090c0b043f68803576573c7b89a5d4b2d9742308
Hsun-Pei Wang <garfieldduck@gmail.com>
2024-12-08 18:49:36 +0100

    💄 : set up the chat overflow

--- src/components/PhoneContent.tsx
+++ src/components/PhoneContent.tsx
@@ -4,8 +4,16 @@ import { LucasChat } from './LucasChat';
 export const PhoneContent = () => {
   return (
     <div className="h-full w-full bg-gray-200 px-4 py-10">
-      <EmmaChat time="9:30" message="You were the Chosen One!" />
-      <LucasChat time="9:32" message="From Daisy UI!" />
+      <div className="h-full w-full overflow-hidden">
+        <div className="h-1/2"></div>
+        <EmmaChat time="9:30" message="Lorem ipsum dolor sit amet" />
+        <LucasChat time="9:32" message="consectetur adipiscing elit" />
+        <EmmaChat time="9:30" message="sed do eiusmod tempor incididunt" />
+        <LucasChat time="9:32" message="labore et dolore magna aliqua" />
+        <LucasChat time="9:32" message="Ut enim ad minim veniam" />
+        <EmmaChat time="9:30" message="quis nostrud exercitation" />
+        <LucasChat time="9:32" message="ullamco laboris nisi " />
+      </div>
     </div>
   );
 };
