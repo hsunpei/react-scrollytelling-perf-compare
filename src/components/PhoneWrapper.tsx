cb0b5dc77ad1b6ed721758019b2e0c0329832cd4
Hsun-Pei Wang <garfieldduck@gmail.com>
2024-12-08 19:19:07 +0100

    💄 : set up the responsive phone template

--- src/components/PhoneWrapper.tsx
+++ src/components/PhoneWrapper.tsx
@@ -1,8 +1,18 @@
+import { useResizeObserver } from '../hooks/useResizeObserver';
+
 export const PhoneWrapper = ({ children }: { children: React.ReactNode }) => {
+  const {
+    ref,
+    dimensions: { height },
+  } = useResizeObserver();
+
   return (
-    <div className="h-full w-full">
-      <div className="flex h-full w-full items-center justify-center lg:w-1/2">
-        <div className="relative flex h-[820px] w-[430px] justify-center overflow-hidden rounded-2xl border-8 border-slate-600 drop-shadow-2xl">
+    <div className="h-full w-full py-20 lg:w-1/2">
+      <div className="flex h-full w-full items-center justify-center" ref={ref}>
+        <div
+          className="relative flex max-w-xl justify-center overflow-hidden rounded-2xl border-8 border-slate-600 drop-shadow-2xl"
+          style={{ height: `${height}px`, width: `${height * 0.49}px` }}
+        >
           <span className="absolute left-1/2 top-5 h-3 w-3 -translate-x-1/2 -translate-y-1/2 transform rounded-full border border-slate-600 bg-slate-600"></span>
           <span className="absolute -right-3 top-20 h-10 rounded-md border-4 border-slate-600"></span>
           <span className="absolute -right-3 top-44 h-24 rounded-md border-4 border-slate-600"></span>
---
+++ src/hooks/useResizeObserver.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useResizeObserver = () => {
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      if (entries[0]) {
+        const { width, height } = entries[0].contentRect;
+        setDimensions({ width, height });
+      }
+    });
+
+    if (ref.current) {
+      resizeObserver.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        resizeObserver.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return { ref, dimensions };
+};
