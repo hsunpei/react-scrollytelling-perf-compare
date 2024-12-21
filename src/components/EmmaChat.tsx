3e4f7d2770e482e07b6533cc801b3de115a6720e
Hsun-Pei Wang <garfieldduck@gmail.com>
2024-12-21 22:50:57 +0100

    ✨ Add AnimatedFadeIn

--- src/components/EmmaChat.tsx
+++ src/components/EmmaChat.tsx
@@ -1,25 +1,30 @@
 import { forwardRef } from 'react';
 
+import { AnimatedFadeIn } from './AnimatedFadeIn';
+
 interface EmmaChatProps {
   time: string;
   message: string;
+  show?: boolean;
 }
 
 export const EmmaChat = forwardRef<HTMLDivElement | null, EmmaChatProps>(
-  ({ time, message }, ref) => {
+  ({ time, message, show }, ref) => {
     return (
-      <div ref={ref} className="chat chat-start mb-2 px-2">
-        <div className="avatar chat-image">
-          <div className="w-10 rounded-full bg-blue-600">
-            <img alt="Tailwind CSS chat bubble component" src="/images/avatar_51.png" />
+      <AnimatedFadeIn start={!!show} initialXPosition={0} initialYPosition={-100} delay={10}>
+        <div ref={ref} className="chat chat-start mb-2 px-2">
+          <div className="avatar chat-image">
+            <div className="w-10 rounded-full bg-blue-600">
+              <img alt="Tailwind CSS chat bubble component" src="/images/avatar_51.png" />
+            </div>
           </div>
+          <div className="chat-header">
+            Emma
+            <time className="ml-2 text-xs opacity-50">{time}</time>
+          </div>
+          <div className="chat-bubble">{message}</div>
         </div>
-        <div className="chat-header">
-          Emma
-          <time className="ml-2 text-xs opacity-50">{time}</time>
-        </div>
-        <div className="chat-bubble">{message}</div>
-      </div>
+      </AnimatedFadeIn>
     );
   }
 );
